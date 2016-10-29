24ddcd92001ce5f817c43c59c7ae65093fa61a59
Rinaldi Utomo <github@rinaldiutomo.com>
2016-10-29 16:27:27 -0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ru_sabnzbd'
+maintainer       'Rinaldi Utomo'
+maintainer_email 'github@rinaldiutomo.com'
+license          'Apache 2.0'
+description      'Installs/Configures ru_sabnzbd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
